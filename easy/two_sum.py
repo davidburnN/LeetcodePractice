30cf938cd6b1989ef89af7b29d42f69711317b5f
david <qwe1029123@gmail.com>
2022-08-25 16:07:32 +0800

    modified two_sum

--- easy/two_sum.py
+++ easy/two_sum.py
@@ -1,4 +1,7 @@
 #! /Users/sehuang/opt/anaconda3/bin/python3.8
 
-print(123)
-print(456)
+class Solution:
+	def twoSum(self, nums: List[int], target: int) -> List[int]:
+		print(123)
+
+Solution.twoSum('test', [1,2,3], 4)
