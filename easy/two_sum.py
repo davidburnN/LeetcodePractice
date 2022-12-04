bbce8a8e809aac4b87b7c4a537bb89b35687fd15
david <qwe1029123@gmail.com>
2022-12-04 17:18:40 +0800

    modified two_sum.py

--- easy/two_sum.py
+++ easy/two_sum.py
@@ -1,4 +1,10 @@
 #! /Users/sehuang/opt/anaconda3/bin/python3.8
 
-print(123)
-print(456)
+class Solution(object):
+    def twoSum(self, nums, target):
+        tot = nums + target
+        return tot
+
+sol1 = Solution().twoSum(5,4)
+
+print(sol1)
