35861e14b72b3d912e02f4488a0fcf5ea9d1b88a
david <qwe1029123@gmail.com>
2023-05-11 22:18:18 +0800

    create SmallestIntegerDivisibleByK.py

---
+++ medium/SmallestIntegerDivisibleByK.py
@@ -0,0 +1,34 @@
+from tqdm import trange
+
+class Solution:
+    def smallestRepunitDivByK(self, k: int) -> int:
+        kStr = f'{k}'
+        kStrLen = len(kStr)
+        nStart = int(f'{"1"*kStrLen}')
+        remainder = nStart%k
+        remainderList = [remainder]
+        if remainder == 0:
+            return kStrLen
+        for i in trange(k):
+            remainder = (remainder*10+1)%k
+            if remainder == 0:
+                return kStrLen+1
+            elif remainder in remainderList:
+                return -1
+            else:
+                kStrLen += 1
+                remainderList.append(remainder)
+        return -1
+    
+sol = Solution()
+testn = 49993
+N = sol.smallestRepunitDivByK(testn)
+print(N)
+print(N%testn)
+            
+
+
+
+
+sol = Solution()
+# print(sol.smallestRepunitDivByK(12357))
