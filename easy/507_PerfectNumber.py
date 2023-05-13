6ca218473aa13c71c1d7d25e4ae30f1b0e8c1813
david <qwe1029123@gmail.com>
2023-05-13 16:55:30 +0800

    modify PerfectNumber to correct version

--- easy/507_PerfectNumber.py
+++ easy/507_PerfectNumber.py
@@ -4,17 +4,20 @@ class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
         result = False
         assert num>=1 & num<=10**8, 'input number should be between 1 and a million'
-        n = 0
+        
+        factorList = [1]
         for i in tqdm(range(int(num/2))):
-            n += num/(i+2)+(i+2) if num%(i+2)==0 else 0
-            if n>num:
-                return False
-        if n == num:
+            if (num%(i+2)==0) & ((i+2) not in factorList):
+                factorList.append(i+2)
+                factorList.append(num/(i+2))
+            if sum(factorList)>num:
+                return result
+        if sum(factorList) == num:
             return True
         else:
             return False
 
 
 sol = Solution()
-sol2 = sol.checkPerfectNumber(496)
+sol2 = sol.checkPerfectNumber(33550336)
 print(sol2)
