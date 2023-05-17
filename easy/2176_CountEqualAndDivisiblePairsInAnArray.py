7d19502d9b7ff2a9b7edfe496e7d8f5f006836e8
david <qwe1029123@gmail.com>
2023-05-17 20:57:09 +0800

    finnaly the right answer to 2176

--- easy/2176_CountEqualAndDivisiblePairsInAnArray.py
+++ easy/2176_CountEqualAndDivisiblePairsInAnArray.py
@@ -33,7 +33,24 @@ class Solution:
         for i in divNumList:
             pairs -= counts(i)
         return int(pairs) 
+    
+    def countPairs_vial(self, nums, k) -> int:
+        numsl = len(nums)
+        nums = np.array(nums)
+        assert numsl>=1 and numsl<=100, 'length of array should be between 1 and 100.'
+        assert (nums>=1).all() and (nums<=100).all(), 'integer in array should be between 1 and 100.'
+        assert k>=1 and k<=100, 'k should be between 1 and 100.'
+        
+        pairs = 0
+        for i in range(numsl):
+            n = nums[i]
+            for j in range(numsl-i-1):
+                if (n == nums[i+j+1]) and ((i*(i+j+1))%k==0):
+                    pairs += 1
+        return pairs
 
 sol = Solution()
-pairs = sol.countPairs([10,2,3,4,9,6,3,10,3,6,3,9,1], 4)
+pairs = sol.countPairs_vial([10, 2, 3, 4, 9, 6, 3,10, 3, 6, 3, 9, 1], 4)
+# pairs = sol.countPairs_vi([ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12], 4)
 print(pairs)
+print((5*(9))%4==0)
