66f68cab713427ee4cfeff6a83fd6a90695e5bc5
david <qwe1029123@gmail.com>
2023-05-23 20:47:00 +0800

    finished problem 22

--- easy/22_GenerateParentheses.py
+++ easy/22_GenerateParentheses.py
@@ -1,20 +1,35 @@
+from tqdm import trange
+
 class Solution:
     def generateParenthesis(self, n):
         assert n>=1 and n<=8, 'input n should be between 1 and 8'
 
         resultList = []
+        resultnumList = []
 
-        for i in range(2**n-2):
+        for i in trange(2**(2*n)-2):
             combine = bin(i+1)[2:]
             combine = f'{int(combine):016d}'
             combine = combine[int(16-2*n):]
             if combine.count('1')!=n:
                 continue
+            invalid = 0
+            res = ''
+            for j in combine:
+                if j == '0':
+                    invalid += 1
+                    res += '('
+                elif j == '1':
+                    invalid -= 1
+                    res += ')'
+                if invalid<0:
+                    res = ''
+                    break
+            if res:
+                resultList.append(res)
+                resultnumList.append(combine)
 
-
-
-
-        return resultList
+        return resultList, resultnumList
 
 def factorial(n):
     if n <= 1:
@@ -22,13 +37,7 @@ def factorial(n):
     else:
         return n*factorial(n-1)
     
-# sol = Solution()
-# gen = sol.generateParenthesis(8)
-# print(gen)
-test = bin(35)
-test = test[2:]
-test = int(test)
-test = f'{test:08d}'
-print(test)
-print(test.count('0'))
-print(2**16)
+sol = Solution()
+gen, gen2 = sol.generateParenthesis(8)
+print(gen)
+print(gen2)
